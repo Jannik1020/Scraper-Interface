79ecbd4a148d00b640238bf825d355196520ea8f
Jannik1020 <jannik.thiele.official@gmail.com>
2022-05-06 20:02:42 +0200

    fix

--- web-interface/api/date/[date].js
+++ web-interface/api/date/[date].js
@@ -1,10 +1,11 @@
 export default (request, response) => {
     const Pool = require('pg-pool');
+    console.log(require("../_utils.js").config())
     const pool = new Pool(require("../_utils.js").config());
 
     var date = request.query.date
-
-    var detail_table = await pool.query('SELECT * from big_table.d' + request.query.date) 
+    console.log(date)
+    var detail_table = await pool.query('SELECT * from big_table.d' + date) 
 
     response.status(200).json({
         body: detail_table,
