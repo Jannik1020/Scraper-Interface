63f455c2411c334c769bebed2b8f7e05e82a68cf
Jannik1020 <jannik.thiele.official@gmail.com>
2022-05-06 19:45:27 +0200

    test

--- web-interface/src/App.js
+++ web-interface/src/App.js
@@ -2,8 +2,19 @@
 import './App.css';
 
 function App() {
+  const data = await fetch("../../../api/date?date=2022_05_06", {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((res) => res.json())
+    .catch((err) => {
+      console.log("Failed logging in");
+      console.log(err);
+    });
+  console.log(data.body)
   return (
     <div className="App">
+      
     </div>
   );
 }
