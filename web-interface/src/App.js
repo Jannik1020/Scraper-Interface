ee6be042563d8b9f3eb283da7bf9b4c7b18cd89b
Jannik1020 <jannik.thiele.official@gmail.com>
2022-07-24 20:52:18 +0200

    interface

--- web-interface/src/App.js
+++ web-interface/src/App.js
@@ -1,22 +1,42 @@
+import "./App.css";
+import { useEffect } from "react";
 
-import './App.css';
 
-async function App() {
-  const date = new Date();
-  const dateString = date.toISOString().split('T')[0].replace(/-/g, "_")
-  const data = await fetch("../../../api/date/" + dateString, {
-    method: "GET",
-    headers: { "Content-Type": "application/json" },
-  })
-    .then((res) => res.json())
-    .catch((err) => {
-      console.log("Failed logging in");
-      console.log(err);
-    });
-  console.log(data.body)
+function App(props) {
+  useEffect(() => {
+    async function getDate() {
+      const date = new Date();
+      const dateString = date.toISOString().split("T")[0].replace(/-/g, "_");
+      const data = await fetch(
+        "https://scraper-interface.vercel.app/api/date/" + dateString,
+        {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        }
+      )
+        .then((res) => res.json())
+        .catch((err) => {
+          console.log("Failed logging in");
+          console.log(err);
+        });
+      console.log(data.body);
+    }
+    getDate();
+  }, []);
+  console.log(props.min.substring(1).replace(/_/g, "-"))
   return (
+    
     <div className="App">
-      {data}
+      <input
+        type="date"
+        id="start"
+        name="trip-start"
+        min={props.min.substring(1).replace(/_/g, "-")}
+        max={props.max.substring(1).replace("_", /-/g)}
+      ></input>
+      <button>
+        Bestätigen
+      </button>
     </div>
   );
 }
--- web-interface/src/index.js
+++ web-interface/src/index.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+var max_date;
+var min_date;
+async function init() {
+  return await fetch(
+    "https://scraper-interface.vercel.app/api/date/range",
+    {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    }
+  )
+    .then((res) => res.json())
+    .catch((err) => {
+      console.log("Failed getting table range");
+      console.log(err);
+    });
+}
+init().then(data => {
+  console.log(data)
 root.render(
   <React.StrictMode>
-    <App />
+    <App max={data.body[data.body.length - 1]} min={data.body[0]} />
   </React.StrictMode>
-);
-
-
+)});
